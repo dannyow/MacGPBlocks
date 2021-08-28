299bfe376366edfeb090673f6b1684317f46c29d
Daniel <danny@buffered.me>
2021-08-28 02:06:15 +0200

    Refactored version with better request status handling

--- MacGPBlocks/smallvm/networkPrims.c
+++ MacGPBlocks/smallvm/networkPrims.c
@@ -2,22 +2,31 @@
 // networkPrims.c
 // Primitives for network communication.
 // Created by Daniel Owsiański on 26/08/2021.
-//   
+//
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h> /* assert() */
 
 #include <curl/curl.h>
 
 #include "mem.h"
 #include "interp.h"
 
+/*
+ Test files:
+ * Very large (26M) https://raw.githubusercontent.com/json-iterator/test-data/master/large-file.json
+ * Large (660k) https://data.cityofnewyork.us/api/views/82rt-zc4y/rows.json
+ */
 
 static CURLM *curlMultiHandle = NULL;
 #define MAX_PARALLEL 10 /* number of simultaneous transfers */
 
-typedef enum {IN_PROGRESS, DONE, FAILED} FetchStatus;
+typedef enum { IN_PROGRESS,
+               DONE,
+               FAILED,
+               CANCELLED } FetchStatus;
 
 typedef struct {
     int id;
@@ -33,11 +42,10 @@ static int nextFetchID = 100;
 
 size_t fetchWriteDataCallback(void *buffer, size_t size, size_t nmemb, void *userData) {
     size_t realSize = size * nmemb;
-    FetchRequest *request = ( FetchRequest *)userData;
-
+    FetchRequest *request = (FetchRequest *)userData;
 
     char *ptr = realloc(request->data, request->byteCount + realSize + 1);
-    if(!ptr) {
+    if (!ptr) {
         /* out of memory! */
         printf("not enough memory (realloc returned NULL)\n");
         return 0;
@@ -48,13 +56,12 @@ size_t fetchWriteDataCallback(void *buffer, size_t size, size_t nmemb, void *use
     request->byteCount += realSize;
     request->data[request->byteCount] = 0;
 
-
-//    printf("*** Data: %ul %ul\n", size, nmemb);
+    //    printf("*** Data: %ul %ul\n", size, nmemb);
 
     return realSize;
 }
 
-static int findRequestWithID(int requestID){
+static int findRequestWithID(int requestID) {
     int i;
     for (i = 0; i < MAX_REQUESTS; i++) {
         if (requests[i].id == requestID) break;
@@ -62,16 +69,62 @@ static int findRequestWithID(int requestID){
     return (i >= MAX_REQUESTS) ? -1 : i;
 }
 
+static void cleanupRequestAtIndex(int index) {
+    if (index < 0 || index > MAX_REQUESTS) return;
+
+    // mark request as free and free the request data, if any
+    requests[index].id = 0;
+    if (requests[index].data) {
+        free(requests[index].data);
+    }
+    requests[index].data = NULL;
+    requests[index].byteCount = 0;
+}
+
+static void processRequestQueue() {
+    int stillAlive = 1;
+    int messagesLeft = -1;
+    CURLMsg *msg = NULL;
+
+    curl_multi_perform(curlMultiHandle, &stillAlive);
+    while ((msg = curl_multi_info_read(curlMultiHandle, &messagesLeft))) {
+        if (msg->msg == CURLMSG_DONE) {
+            int requestID;
+
+            CURLcode msgCode = msg->data.result;
+            CURL *curl = msg->easy_handle;
+            curl_easy_getinfo(curl, CURLINFO_PRIVATE, &requestID);
+
+            int requestIndex = findRequestWithID(requestID);
+            if (requestIndex > -1) {
+                assert(CANCELLED == requests[requestIndex].status || IN_PROGRESS == requests[requestIndex].status);
+
+                if (CANCELLED == requests[requestIndex].status) {
+                    cleanupRequestAtIndex(requestIndex);
+                } else if (msgCode == CURLE_OK) {
+                    requests[requestIndex].status = DONE;
+                } else {
+                    printf("error on requestID: %d %s\n", requestID, curl_easy_strerror(msgCode));
+                    requests[requestIndex].status = FAILED;
+                }
+            }
+
+            curl_multi_remove_handle(curlMultiHandle, curl);
+            curl_easy_cleanup(curl);
+        }
+    }
+}
+
 static OBJ primStartRequest(int nargs, OBJ args[]) {
     if (nargs < 1) return notEnoughArgsFailure();
     OBJ url = args[0];
     if (NOT_CLASS(url, StringClass)) return primFailed("First argument must be a string");
 
-    if(curlMultiHandle == NULL){
+    if (curlMultiHandle == NULL) {
         curl_global_init(CURL_GLOBAL_ALL);
         curlMultiHandle = curl_multi_init();
 
-        if(!curlMultiHandle){
+        if (!curlMultiHandle) {
             return primFailed("Could not initate network connection");
         }
 
@@ -92,7 +145,7 @@ static OBJ primStartRequest(int nargs, OBJ args[]) {
             break;
         }
     }
-    if (i >= MAX_REQUESTS) return nilObj; // no free request slots (unlikely)
+    if (i >= MAX_REQUESTS) return nilObj;  // no free request slots (unlikely)
 
     CURL *curl = curl_easy_init();
     if (!curl) {
@@ -108,19 +161,18 @@ static OBJ primStartRequest(int nargs, OBJ args[]) {
     // headers = curl_slist_append(headers, "Accept:");
     headers = curl_slist_append(headers, "Accept: application/json");
     headers = curl_slist_append(headers, "Content-Type: application/json; charset=utf-8");
-    // headers = curl_slist_append(headers, "Authorization: token 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");
 
     curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
 
     curl_easy_setopt(curl, CURLOPT_URL, obj2str(url));
-//    curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+    //    curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
 
     curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
     curl_easy_setopt(curl, CURLOPT_USERAGENT, "libcurl-agent/1.0");
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, fetchWriteDataCallback);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&requests[i]);
     curl_easy_setopt(curl, CURLOPT_PRIVATE, requests[i].id);
-//    curl_easy_setopt(curl, CURLOPT_TIMEOUT_MS, 1000L);
+    //    curl_easy_setopt(curl, CURLOPT_TIMEOUT_MS, 1000L);
 
     curl_multi_add_handle(curlMultiHandle, curl);
     printf("🟢 Request ready to run for URL: %s ID: %d (at: %d)\n", obj2str(url), requests[i].id, i);
@@ -128,127 +180,77 @@ static OBJ primStartRequest(int nargs, OBJ args[]) {
     return int2obj(requests[i].id);
 }
 
+// Returns a BinaryData object on success, false on failure, and nil when fetch is still in progress.
 static OBJ primFetchRequestResult(int nargs, OBJ args[]) {
-    // Returns a BinaryData object on success, false on failure, and nil when fetch is still in progress.
+    // https://stackoverflow.com/questions/43662072/anyway-to-avoid-to-loop-monitor-multi-handle-in-libcurl
+    //  https://stackoverflow.com/questions/48644383/libcurl-progress-callback-not-working-with-multi
+    // https://stackoverflow.com/questions/24413806/libuv-event-loop-and-libcurl
+
+    // First process requests from libcurl queue
+    processRequestQueue();
+
     if (nargs < 1) return notEnoughArgsFailure();
     if (!isInt(args[0])) return primFailed("Expected integer");
     int id = obj2int(args[0]);
 
     // find the fetch request with the given id
     int i = findRequestWithID(id);
-    if (i < 0) return falseObj; // could not find request with id; report as failure
+    if (i < 0) return falseObj;  // could not find request with id; report as failure
 
-    if(!curlMultiHandle){
+    if (!curlMultiHandle) {
         return falseObj;
     }
 
-    int stillAlive = 1;
-    int messagesLeft = -1;
-    CURLMsg *msg = NULL;
-
-    CURLMcode r;
-
     OBJ result = nilObj;
-//    do {
+    if (IN_PROGRESS == requests[i].status) {
+        return result;
+    }
 
-        r = curl_multi_perform(curlMultiHandle, &stillAlive);
-//    printf("🟨 code: %d (%s)", r, curl_multi_strerror(r));
+    result = falseObj;
 
-        while((msg = curl_multi_info_read(curlMultiHandle, &messagesLeft))) {
-            if(msg->msg == CURLMSG_DONE) {
-
-                int requestID;
-                CURL *curl = msg->easy_handle;
-                CURLcode msgCode = (CURLcode)msg->data.result;
-                curl_easy_getinfo(curl, CURLINFO_PRIVATE, &requestID);
-
-
-                long responseCode = 0;
-                curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &responseCode);
-
-
-                printf("🔈 Response: %ld  Error? %d %s\n", responseCode, msg->data.result, curl_easy_strerror(msgCode));
-
-
-                if(id == (requestID)){
-                    int byteCount = requests[i].byteCount;
-                    result = newBinaryData(byteCount);
-                    if (result) {
-                        memmove(&FIELD(result, 0), requests[i].data, byteCount);
-                    } else {
-                        printf("Insufficient memory for requested file (%ul bytes needed); skipping.\n", byteCount);
-                    }
-
-                    // mark request as free and free the request data, if any
-                    requests[i].id = 0;
-                    if (requests[i].data){
-                        free(requests[i].data);
-                    }
-                    requests[i].status = DONE;
-                    requests[i].data = NULL;
-                    requests[i].byteCount = 0;
-                }else{
-                    printf("🟢 Got message but with ID: %d instead of %d\n", requestID, id);
-                }
-
-                //            fprintf(stderr, "R: %d - %s <%s>\n",
-                //                    msg->data.result, curl_easy_strerror(msg->data.result), url);
-                curl_multi_remove_handle(curlMultiHandle, curl);
-                curl_easy_cleanup(curl);
-            }
-            else {
-                fprintf(stderr, "🛑E: CURLMsg (%d)\n", msg->msg);
-            }
-
+    if (DONE == requests[i].status && requests[i].data) {
+        // allocate result object
+        int byteCount = requests[i].byteCount;
+        result = newBinaryData(byteCount);
+        if (result) {
+            memmove(&FIELD(result, 0), requests[i].data, byteCount);
+        } else {
+            printf("Insufficient memory for requested file (%ul bytes needed); skipping.\n", byteCount);
         }
-//        if(stillAlive){
-//            curl_multi_wait(curlMultiHandle, NULL, 0, 1000, NULL);
-//        }
-//    } while(stillAlive);
+    }
 
-    printf("primFetchRestResult id=%d result is [%s] \n", id, (result == nilObj)?"nil":"not nil");
-//    dumpObj(result);
+    cleanupRequestAtIndex(i);
     return result;
-
-    //
-    //    if (IN_PROGRESS == requests[i].status) return nilObj; // in progress
-    //
-    //    OBJ result = falseObj;
-    //
-    //    if (DONE == requests[i].status && requests[i].data) {
-    //        // allocate result object
-    //        int byteCount = requests[i].byteCount;
-    //        result = newBinaryData(byteCount);
-    //        if (result) {
-    //            memmove(&FIELD(result, 0), requests[i].data, byteCount);
-    //        } else {
-    //            printf("Insufficient memory for requested file (%ul bytes needed); skipping.\n", byteCount);
-    //        }
-    //    }
-    //
-    //    // mark request as free and free the request data, if any
-    //    requests[i].id = 0;
-    //    if (requests[i].data) free(requests[i].data);
-    //    requests[i].data = NULL;
-    //    requests[i].byteCount = 0;
-    //
-    //    return result;
 }
 
-static OBJ primCancelRequest(int nargs, OBJ args[]){
-    printf("🔴 primCancelRequest \n");
-    // TODO: search for request with the given ID, and set status to CANCELLED? plus free memory if there is any
+static OBJ primCancelRequest(int nargs, OBJ args[]) {
+    if (nargs < 1) return notEnoughArgsFailure();
+    if (!isInt(args[0])) return primFailed("Expected integer");
+    int id = obj2int(args[0]);
+
+    // find the fetch request with the given id
+    int i = findRequestWithID(id);
+    if (i < 0) return nilObj;
+
+    printf("🔴 primCancelRequest with ID: %d (%d)\n", id, i);
+    if (IN_PROGRESS == requests[i].status) {
+        requests[i].status = CANCELLED;
+    } else {
+        printf("?? can't cancel a request that is not in progress (%d), requestID: %d (%d)\n", requests[i].status, id, i);
+    }
+
+    processRequestQueue();
     return nilObj;
 }
 
 static PrimEntry networkPrimList[] = {
     {"-----", NULL, "Network: HTTP/HTTPS"},
-    {"startRequest",  primStartRequest,     "Start downloading the contents of a URL. Return an id that can be used to get the result. Argument: urlString"},
-    {"fetchRequestResult", primFetchRequestResult,    "Return the result of the fetch operation with the given id: a BinaryData object (success), false (failure), or nil if in progress. Argument: id"},
-    {"cancelRequest", primCancelRequest,    "Cancel the request with the given id. Argument: id"},
+    {"startRequest", primStartRequest, "Start downloading the contents of a URL. Return an id that can be used to get the result. Argument: urlString"},
+    {"fetchRequestResult", primFetchRequestResult, "Return the result of the fetch operation with the given id: a BinaryData object (success), false (failure), or nil if in progress. Argument: id"},
+    {"cancelRequest", primCancelRequest, "Cancel the request with the given id. Argument: id"},
 };
 
-PrimEntry* networkPrimitives(int *primCount) {
+PrimEntry *networkPrimitives(int *primCount) {
     *primCount = sizeof(networkPrimList) / sizeof(PrimEntry);
     return networkPrimList;
 }
