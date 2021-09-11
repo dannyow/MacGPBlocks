6148041bd7bda22f8f04af183dbd3ed9f92a2a05
Daniel <danny@buffered.me>
2021-09-11 16:28:59 +0200

    refactored version

--- MacGPBlocks/smallvm/httpPrims.c
+++ MacGPBlocks/smallvm/httpPrims.c
@@ -36,6 +36,9 @@ typedef struct {
 } FetchRequest;
 
 #define MAX_REQUESTS 100
+#define DEFAULT_REQUEST_TIMEOUT 10000
+#define DEFAULT_USER_AGENT "gp-blocks"
+
 FetchRequest requests[MAX_REQUESTS];
 
 static int nextFetchID = 100;
@@ -120,7 +123,7 @@ static void processRequestQueue() {
     }
 }
 
-static OBJ primStartRequest(int nargs, OBJ args[]) {
+static OBJ primStartRequestORG(int nargs, OBJ args[]) {
     if (nargs < 1) return notEnoughArgsFailure();
     OBJ url = args[0];
     if (NOT_CLASS(url, StringClass)) return primFailed("First argument must be a string");
@@ -185,13 +188,107 @@ static OBJ primStartRequest(int nargs, OBJ args[]) {
     return int2obj(requests[i].id);
 }
 
+static OBJ startRequest(int requestIndex, const char *url, const char *method, OBJ headersArray, const char *postBodyString, long timeout) {
+    if (requestIndex >= MAX_REQUESTS) return nilObj;
+
+    if (curlMultiHandle == NULL) {
+        curl_global_init(CURL_GLOBAL_ALL);
+        curlMultiHandle = curl_multi_init();
+
+        if (!curlMultiHandle) {
+            return primFailed("Could not initate network connection");
+        }
+
+        /* Limit the amount of simultaneous connections curl should allow: */
+        curl_multi_setopt(curlMultiHandle, CURLMOPT_MAXCONNECTS, (long)MAX_PARALLEL);
+    }
+
+    CURL *curl = curl_easy_init();
+    if (!curl) {
+        return primFailed("Could not initate network connection");
+    }
+
+    //https://curl.se/libcurl/c/curl_easy_setopt.html
+    curl_easy_setopt(curl, CURLOPT_URL, url);
+    curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, method);
+    //curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+    curl_easy_setopt(curl, CURLOPT_USERAGENT, DEFAULT_USER_AGENT); //can overwritten by headers
+    curl_easy_setopt(curl, CURLOPT_TIMEOUT_MS, timeout);
+
+    if (headersArray != nilObj) {
+        struct curl_slist *headers = NULL;
+        int count = WORDS(headersArray);
+        for (int i = 0; i < count; i++) {
+            OBJ obj = FIELD(headersArray, i);
+            headers = curl_slist_append(headers, obj2str(obj));
+        }
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+    }
+
+    if (postBodyString){
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, postBodyString);
+    }
+
+    FetchRequest *request = &requests[requestIndex];
+
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, fetchWriteDataCallback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)request);
+    curl_easy_setopt(curl, CURLOPT_PRIVATE, request->id);
+    curl_multi_add_handle(curlMultiHandle, curl);
+
+    printf("🛫 Request ready to start for URL: >%s< ID: [%d] (at: [%d])\n", url, request->id, requestIndex);
+
+    return int2obj(request->id);
+}
+
+// Returns request id on success, nil when there is no free slots for a new request (unlikely)
+static OBJ primStartRequest(int nargs, OBJ args[]) {
+    if (nargs < 5) return notEnoughArgsFailure();
+    OBJ url = args[0];
+    if (NOT_CLASS(url, StringClass)) return primFailed("URL argument must be a string");
+
+    OBJ method = args[1];
+    if (NOT_CLASS(method, StringClass)) return primFailed("HTTP method argument must be a string");
+
+    OBJ headersArray = args[2];
+    if (headersArray != nilObj && NOT_CLASS(headersArray, ArrayClass)) return primFailed("Headers argument must be an array");
+
+    OBJ bodyObj = args[3];
+    char *bodyString = NULL;
+    if (bodyObj != nilObj) {
+        if (NOT_CLASS(bodyObj, StringClass)) return primFailed("Body argument must be string");
+        bodyString = obj2str(bodyObj);
+    }
+
+    OBJ timeoutObj = args[4];
+    long timeoutVal = DEFAULT_REQUEST_TIMEOUT;
+    if (timeoutObj != nilObj) {
+        if (!isInt(timeoutObj)) return primFailed("Timeout argument must be an integer");
+        timeoutVal = obj2int(timeoutObj);
+    }
+
+    // find an unused request
+    int i;
+    for (i = 0; i < MAX_REQUESTS; i++) {
+        if (!requests[i].id) {
+            requests[i].id = nextFetchID++;
+            requests[i].status = IN_PROGRESS;
+            requests[i].data = NULL;
+            requests[i].byteCount = 0;
+
+            break;
+        }
+    }
+    if (i >= MAX_REQUESTS) return nilObj;  // no free request slots (unlikely)
+
+    return startRequest(i, obj2str(url), obj2str(method), headersArray, bodyString, timeoutVal);
+}
+
 // Returns a BinaryData object on success, false on failure, and nil when fetch is still in progress.
 static OBJ primFetchRequestResult(int nargs, OBJ args[]) {
-    // https://stackoverflow.com/questions/43662072/anyway-to-avoid-to-loop-monitor-multi-handle-in-libcurl
-    // https://stackoverflow.com/questions/48644383/libcurl-progress-callback-not-working-with-multi
-    // https://stackoverflow.com/questions/24413806/libuv-event-loop-and-libcurl
 
-    // First process requests from libcurl queue
+    // First use this opportunity to process requests from the queue (for libcurl)
     processRequestQueue();
 
     if (nargs < 1) return notEnoughArgsFailure();
@@ -246,7 +343,7 @@ static OBJ primCancelRequest(int nargs, OBJ args[]) {
 
 static PrimEntry httpPrimList[] = {
     {"-----", NULL, "Network: HTTP/HTTPS"},
-    {"startRequest", primStartRequest, "Start downloading the contents of a URL. Return an id that can be used to get the result. Argument: urlString"},
+    {"startRequest", primStartRequest, "Start downloading the contents of a URL. Return an id that can be used to get the result. Arguments: urlString, methodString, headersArrayOrNil, postBodyStringOrNil, timeoutIntegerOrNil"},
     {"fetchRequestResult", primFetchRequestResult, "Return the result of the fetch operation with the given id: a BinaryData object (success), false (failure), or nil if in progress. Argument: id"},
     {"cancelRequest", primCancelRequest, "Cancel the request (if exists) with the given id. Argument: id"},
 };
